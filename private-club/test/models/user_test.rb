e458b0acd23bfb07a243132c3ccb1cfc9d37842c
tomroumeg <tomroumeg@gmail.com>
2018-11-05 22:51:49 +0100

    first commit

---
+++ private-club/test/models/user_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+	def setup
+		@user = User.new(name: "John", last_name:"Doe", email: "john@mail.com",
+	                 	 password: "password", password_confirmation: "password")
+	end
+
+	test "password should be present (nonblank)" do
+	    @user.password = @user.password_confirmation = " " * 6
+	    assert_not @user.valid?
+	  end
+
+	  test "password should have a minimum length" do
+	    @user.password = @user.password_confirmation = "a" * 5
+	    assert_not @user.valid?
+	 end
+
+	 test "name should be present" do
+	 	@user.name = ""
+	 	assert_not @user.valid?
+	 end
+
+	 test "email adresses should be unique" do
+	 	duplicate_user = @user.dup
+	 	@user.save
+	 	assert_not duplicate_user.valid?
+	 end
+
+end
